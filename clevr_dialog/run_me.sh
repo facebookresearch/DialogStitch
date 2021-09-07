84f600833cc9ae599dd94bd794be44798f63d609
Satwik Kottur <satwik27@gmail.com>
2021-09-07 14:59:37 -0700

    Adds header to run_me.sh

--- clevr_dialog/run_me.sh
+++ clevr_dialog/run_me.sh
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+# 
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 # Download CLEVR images.
 # pip install gdown
 # wget https://dl.fbaipublicfiles.com/clevr/CLEVR_v1.0.zip data/
